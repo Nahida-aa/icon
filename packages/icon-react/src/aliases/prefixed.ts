3b6e335609f5fb344d3b329c65d3312979559438
aa <Nahida-aa@outlook.com>
2026-04-27 17:09:52 +0800

    fix

--- packages/icon-react/src/aliases/prefixed.ts
+++ packages/icon-react/src/aliases/prefixed.ts
@@ -2,108 +2,108 @@
 export { default as XaaAArrowDown } from '../icons/a-arrow-down';
 // ArrowUp aliases
 export { default as XaaArrowUp } from '../icons/arrow-up';
-// Attachment aliases
-export { default as XaaAttachment } from '../icons/attachment';
-// Box aliases
-export { default as XaaBox } from '../icons/box';
-// Bot aliases
-export { default as XaaBot } from '../icons/bot';
-// CheckedSquare aliases
-export { default as XaaCheckedSquare } from '../icons/checked-square';
-// ChevronDown aliases
-export { default as XaaChevronDown } from '../icons/chevron-down';
-// Copy aliases
-export { default as XaaCopy } from '../icons/copy';
-// Anthropic aliases
-export { default as XaaAnthropic } from '../icons/anthropic';
-// Home aliases
-export { default as XaaHome } from '../icons/home';
-// DocumentText aliases
-export { default as XaaDocumentText } from '../icons/document-text';
 // CheckCircleFill aliases
 export { default as XaaCheckCircleFill } from '../icons/check-circle-fill';
+// Box aliases
+export { default as XaaBox } from '../icons/box';
+// Anthropic aliases
+export { default as XaaAnthropic } from '../icons/anthropic';
+// ChevronDown aliases
+export { default as XaaChevronDown } from '../icons/chevron-down';
+// Attachment aliases
+export { default as XaaAttachment } from '../icons/attachment';
+// CheckedSquare aliases
+export { default as XaaCheckedSquare } from '../icons/checked-square';
+// DocumentText aliases
+export { default as XaaDocumentText } from '../icons/document-text';
 // Gps aliases
 export { default as XaaGps } from '../icons/gps';
-// Cross aliases
-export { default as XaaCross } from '../icons/cross';
-// Github aliases
-export { default as XaaGithub } from '../icons/github';
 // LayoutBottomBar aliases
 export { default as XaaLayoutBottomBar } from '../icons/layout-bottom-bar';
-// Message aliases
-export { default as XaaMessage } from '../icons/message';
-// Mix aliases
-export { default as XaaMix } from '../icons/mix';
+// Bot aliases
+export { default as XaaBot } from '../icons/bot';
+// Menu aliases
+export { default as XaaMenu } from '../icons/menu';
 // MoreHorizontal aliases
 export { default as XaaMoreHorizontal } from '../icons/more-horizontal';
+// Github aliases
+export { default as XaaGithub } from '../icons/github';
 // Loader aliases
 export { default as XaaLoader } from '../icons/loader';
 // Openai aliases
 export { default as XaaOpenai } from '../icons/openai';
-// File aliases
-export { default as XaaFile } from '../icons/file';
-// Menu aliases
-export { default as XaaMenu } from '../icons/menu';
-// Pen aliases
-export { default as XaaPen } from '../icons/pen';
-// Route aliases
-export { default as XaaRoute } from '../icons/route';
-// More aliases
-export { default as XaaMore } from '../icons/more';
-// LayoutBottomBarFilled aliases
-export { default as XaaLayoutBottomBarFilled } from '../icons/layout-bottom-bar-filled';
-// PencilEdit aliases
-export { default as XaaPencilEdit } from '../icons/pencil-edit';
 // Delta aliases
 export { default as XaaDelta } from '../icons/delta';
-// ShieldLock aliases
-export { default as XaaShieldLock } from '../icons/shield-lock';
-// Redo aliases
-export { default as XaaRedo } from '../icons/redo';
-// Summarize aliases
-export { default as XaaSummarize } from '../icons/summarize';
-// ThumbDown aliases
-export { default as XaaThumbDown } from '../icons/thumb-down';
-// Invoice aliases
-export { default as XaaInvoice } from '../icons/invoice';
-// Paperclip aliases
-export { default as XaaPaperclip } from '../icons/paperclip';
-// ThumbUp aliases
-export { default as XaaThumbUp } from '../icons/thumb-up';
-// Stop aliases
-export { default as XaaStop } from '../icons/stop';
-// Plus aliases
-export { default as XaaPlus } from '../icons/plus';
-// Upload aliases
-export { default as XaaUpload } from '../icons/upload';
-// Sparkles1 aliases
-export { default as XaaSparkles1 } from '../icons/sparkles1';
-// Google aliases
-export { default as XaaGoogle } from '../icons/google';
-// Vercel aliases
-export { default as XaaVercel } from '../icons/vercel';
-// UncheckedSquare aliases
-export { default as XaaUncheckedSquare } from '../icons/unchecked-square';
+// Message aliases
+export { default as XaaMessage } from '../icons/message';
+// More aliases
+export { default as XaaMore } from '../icons/more';
+// File aliases
+export { default as XaaFile } from '../icons/file';
+// LayoutBottomBarFilled aliases
+export { default as XaaLayoutBottomBarFilled } from '../icons/layout-bottom-bar-filled';
 // SidebarLeft aliases
 export { default as XaaSidebarLeft } from '../icons/sidebar-left';
+// ShieldLock aliases
+export { default as XaaShieldLock } from '../icons/shield-lock';
+// Cross aliases
+export { default as XaaCross } from '../icons/cross';
+// Mix aliases
+export { default as XaaMix } from '../icons/mix';
 // FingerprintPattern aliases
 export { default as XaaFingerprintPattern } from '../icons/fingerprint-pattern';
 export {
   /** @deprecated The name of this icon was changed because it didn't meet our guidelines anymore, use {@link FingerprintPattern} instead. This alias will be removed in v1.0 */
   default as XaaFingerprint
 } from '../icons/fingerprint-pattern';
-// User aliases
-export { default as XaaUser } from '../icons/user';
-// Sparkles aliases
-export { default as XaaSparkles } from '../icons/sparkles';
 // Info aliases
 export { default as XaaInfo } from '../icons/info';
 // Wakatime aliases
 export { default as XaaWakatime } from '../icons/wakatime';
-// Undo aliases
-export { default as XaaUndo } from '../icons/undo';
+// Route aliases
+export { default as XaaRoute } from '../icons/route';
+// Copy aliases
+export { default as XaaCopy } from '../icons/copy';
+// Plus aliases
+export { default as XaaPlus } from '../icons/plus';
+// Invoice aliases
+export { default as XaaInvoice } from '../icons/invoice';
+// Vercel aliases
+export { default as XaaVercel } from '../icons/vercel';
+// PencilEdit aliases
+export { default as XaaPencilEdit } from '../icons/pencil-edit';
+// Redo aliases
+export { default as XaaRedo } from '../icons/redo';
+// Pen aliases
+export { default as XaaPen } from '../icons/pen';
+// Home aliases
+export { default as XaaHome } from '../icons/home';
+// User aliases
+export { default as XaaUser } from '../icons/user';
+// Paperclip aliases
+export { default as XaaPaperclip } from '../icons/paperclip';
+// Google aliases
+export { default as XaaGoogle } from '../icons/google';
 // UserGroup aliases
 export { default as XaaUserGroup } from '../icons/user-group';
+// Upload aliases
+export { default as XaaUpload } from '../icons/upload';
+// Undo aliases
+export { default as XaaUndo } from '../icons/undo';
+// UncheckedSquare aliases
+export { default as XaaUncheckedSquare } from '../icons/unchecked-square';
 // Trash aliases
 export { default as XaaTrash } from '../icons/trash';
+// ThumbDown aliases
+export { default as XaaThumbDown } from '../icons/thumb-down';
+// ThumbUp aliases
+export { default as XaaThumbUp } from '../icons/thumb-up';
+// Summarize aliases
+export { default as XaaSummarize } from '../icons/summarize';
+// Stop aliases
+export { default as XaaStop } from '../icons/stop';
+// Sparkles aliases
+export { default as XaaSparkles } from '../icons/sparkles';
+// Sparkles1 aliases
+export { default as XaaSparkles1 } from '../icons/sparkles1';
 
--- packages/icon-react/src/aliases/suffixed.ts
+++ packages/icon-react/src/aliases/suffixed.ts
@@ -1,109 +1,109 @@
+// ArrowUp aliases
+export { default as ArrowUpIcon } from '../icons/arrow-up';
+// CheckCircleFill aliases
+export { default as CheckCircleFillIcon } from '../icons/check-circle-fill';
 // AArrowDown aliases
 export { default as AArrowDownIcon } from '../icons/a-arrow-down';
-// Copy aliases
-export { default as CopyIcon } from '../icons/copy';
+// ChevronDown aliases
+export { default as ChevronDownIcon } from '../icons/chevron-down';
+// Gps aliases
+export { default as GpsIcon } from '../icons/gps';
 // Anthropic aliases
 export { default as AnthropicIcon } from '../icons/anthropic';
 // DocumentText aliases
 export { default as DocumentTextIcon } from '../icons/document-text';
-// ChevronDown aliases
-export { default as ChevronDownIcon } from '../icons/chevron-down';
-// CheckCircleFill aliases
-export { default as CheckCircleFillIcon } from '../icons/check-circle-fill';
-// Github aliases
-export { default as GithubIcon } from '../icons/github';
-// Home aliases
-export { default as HomeIcon } from '../icons/home';
-// Message aliases
-export { default as MessageIcon } from '../icons/message';
-// CheckedSquare aliases
-export { default as CheckedSquareIcon } from '../icons/checked-square';
-// Loader aliases
-export { default as LoaderIcon } from '../icons/loader';
-// Cross aliases
-export { default as CrossIcon } from '../icons/cross';
-// LayoutBottomBar aliases
-export { default as LayoutBottomBarIcon } from '../icons/layout-bottom-bar';
-// File aliases
-export { default as FileIcon } from '../icons/file';
-// Gps aliases
-export { default as GpsIcon } from '../icons/gps';
 // MoreHorizontal aliases
 export { default as MoreHorizontalIcon } from '../icons/more-horizontal';
-// Mix aliases
-export { default as MixIcon } from '../icons/mix';
 // Bot aliases
 export { default as BotIcon } from '../icons/bot';
+// LayoutBottomBar aliases
+export { default as LayoutBottomBarIcon } from '../icons/layout-bottom-bar';
 // Openai aliases
 export { default as OpenaiIcon } from '../icons/openai';
-// Route aliases
-export { default as RouteIcon } from '../icons/route';
-// Pen aliases
-export { default as PenIcon } from '../icons/pen';
-// Redo aliases
-export { default as RedoIcon } from '../icons/redo';
-// Invoice aliases
-export { default as InvoiceIcon } from '../icons/invoice';
-// ThumbUp aliases
-export { default as ThumbUpIcon } from '../icons/thumb-up';
-// Attachment aliases
-export { default as AttachmentIcon } from '../icons/attachment';
-// Stop aliases
-export { default as StopIcon } from '../icons/stop';
-// ShieldLock aliases
-export { default as ShieldLockIcon } from '../icons/shield-lock';
-// Menu aliases
-export { default as MenuIcon } from '../icons/menu';
-// ThumbDown aliases
-export { default as ThumbDownIcon } from '../icons/thumb-down';
-// Paperclip aliases
-export { default as PaperclipIcon } from '../icons/paperclip';
-// Sparkles1 aliases
-export { default as Sparkles1Icon } from '../icons/sparkles1';
-// LayoutBottomBarFilled aliases
-export { default as LayoutBottomBarFilledIcon } from '../icons/layout-bottom-bar-filled';
 // Box aliases
 export { default as BoxIcon } from '../icons/box';
-// Upload aliases
-export { default as UploadIcon } from '../icons/upload';
-// Delta aliases
-export { default as DeltaIcon } from '../icons/delta';
-// Summarize aliases
-export { default as SummarizeIcon } from '../icons/summarize';
-// Plus aliases
-export { default as PlusIcon } from '../icons/plus';
+// CheckedSquare aliases
+export { default as CheckedSquareIcon } from '../icons/checked-square';
+// LayoutBottomBarFilled aliases
+export { default as LayoutBottomBarFilledIcon } from '../icons/layout-bottom-bar-filled';
+// Loader aliases
+export { default as LoaderIcon } from '../icons/loader';
+// Menu aliases
+export { default as MenuIcon } from '../icons/menu';
+// SidebarLeft aliases
+export { default as SidebarLeftIcon } from '../icons/sidebar-left';
+// File aliases
+export { default as FileIcon } from '../icons/file';
+// Mix aliases
+export { default as MixIcon } from '../icons/mix';
+// Attachment aliases
+export { default as AttachmentIcon } from '../icons/attachment';
+// Info aliases
+export { default as InfoIcon } from '../icons/info';
+// Github aliases
+export { default as GithubIcon } from '../icons/github';
+// Copy aliases
+export { default as CopyIcon } from '../icons/copy';
+// Cross aliases
+export { default as CrossIcon } from '../icons/cross';
+// More aliases
+export { default as MoreIcon } from '../icons/more';
 // FingerprintPattern aliases
 export { default as FingerprintPatternIcon } from '../icons/fingerprint-pattern';
 export {
   /** @deprecated The name of this icon was changed because it didn't meet our guidelines anymore, use {@link FingerprintPattern} instead. This alias will be removed in v1.0 */
   default as FingerprintIcon
 } from '../icons/fingerprint-pattern';
-// PencilEdit aliases
-export { default as PencilEditIcon } from '../icons/pencil-edit';
-// ArrowUp aliases
-export { default as ArrowUpIcon } from '../icons/arrow-up';
-// UncheckedSquare aliases
-export { default as UncheckedSquareIcon } from '../icons/unchecked-square';
-// More aliases
-export { default as MoreIcon } from '../icons/more';
 // Vercel aliases
 export { default as VercelIcon } from '../icons/vercel';
-// Google aliases
-export { default as GoogleIcon } from '../icons/google';
-// SidebarLeft aliases
-export { default as SidebarLeftIcon } from '../icons/sidebar-left';
+// Route aliases
+export { default as RouteIcon } from '../icons/route';
 // User aliases
 export { default as UserIcon } from '../icons/user';
-// Sparkles aliases
-export { default as SparklesIcon } from '../icons/sparkles';
+// Invoice aliases
+export { default as InvoiceIcon } from '../icons/invoice';
+// ShieldLock aliases
+export { default as ShieldLockIcon } from '../icons/shield-lock';
+// Message aliases
+export { default as MessageIcon } from '../icons/message';
+// Google aliases
+export { default as GoogleIcon } from '../icons/google';
+// Home aliases
+export { default as HomeIcon } from '../icons/home';
 // Wakatime aliases
 export { default as WakatimeIcon } from '../icons/wakatime';
+// Paperclip aliases
+export { default as PaperclipIcon } from '../icons/paperclip';
+// Plus aliases
+export { default as PlusIcon } from '../icons/plus';
 // Undo aliases
 export { default as UndoIcon } from '../icons/undo';
-// Info aliases
-export { default as InfoIcon } from '../icons/info';
+// Delta aliases
+export { default as DeltaIcon } from '../icons/delta';
+// Upload aliases
+export { default as UploadIcon } from '../icons/upload';
+// Pen aliases
+export { default as PenIcon } from '../icons/pen';
+// ThumbDown aliases
+export { default as ThumbDownIcon } from '../icons/thumb-down';
 // UserGroup aliases
 export { default as UserGroupIcon } from '../icons/user-group';
+// Redo aliases
+export { default as RedoIcon } from '../icons/redo';
+// Summarize aliases
+export { default as SummarizeIcon } from '../icons/summarize';
 // Trash aliases
 export { default as TrashIcon } from '../icons/trash';
+// Stop aliases
+export { default as StopIcon } from '../icons/stop';
+// ThumbUp aliases
+export { default as ThumbUpIcon } from '../icons/thumb-up';
+// Sparkles aliases
+export { default as SparklesIcon } from '../icons/sparkles';
+// Sparkles1 aliases
+export { default as Sparkles1Icon } from '../icons/sparkles1';
+// PencilEdit aliases
+export { default as PencilEditIcon } from '../icons/pencil-edit';
+// UncheckedSquare aliases
+export { default as UncheckedSquareIcon } from '../icons/unchecked-square';
 
